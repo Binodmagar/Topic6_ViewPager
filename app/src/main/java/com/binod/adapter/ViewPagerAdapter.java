651c1fd4953039c59c03d14bd7b4f95456cf97ea
Binodmagar <ranabinod615@gmail.com>
2019-12-16 20:08:39 +0545

    [#3] extend FragmentPagerAdapter and written code

--- app/src/main/java/com/binod/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/binod/adapter/ViewPagerAdapter.java
@@ -1,4 +1,45 @@
 package com.binod.adapter;
 
-public class ViewPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<String> fragmentTitle = new ArrayList<>();
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    //overriding to display title of the fragment
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+
+    //add the fragment and title to the list
+    public void addFragment(Fragment fragment, String title){
+        fragmentList.add(fragment);
+        fragmentTitle.add(title);
+    }
+
 }
